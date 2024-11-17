2fd98547a9d8c3a17c0701ec52303b615fe0d381
axviii3 <188557785+axviii3@users.noreply.github.com>
2024-11-18 00:13:25 +0530

    feat: add keymaps to open quickfix list and locatin list in touble

--- lua/config/plugin/quality_of_life/trouble.lua
+++ lua/config/plugin/quality_of_life/trouble.lua
@@ -16,5 +16,16 @@ return {
 			"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
 			{ desc = "plugins/trouble: open trouble diagnostics for current buffer" }
 		)
+
+		NKeymap(
+			"<leader>tq",
+			"<cmd>Trouble qflist toggle<CR>",
+			{ desc = "plugins/trouble: open quick fix list in troubl" }
+		)
+		NKeymap(
+			"<leader>tl",
+			"<cmd>Trouble loclist toggle<CR>",
+			{ desc = "plugins/trouble: open location fix list in troubl" }
+		)
 	end,
 }
