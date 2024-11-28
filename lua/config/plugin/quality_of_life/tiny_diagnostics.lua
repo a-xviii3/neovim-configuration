15ddc96c31cdf8a5740440aa0cd914c898c5d9ea
anir-baaan <188557785+anir-baaan@users.noreply.github.com>
2024-11-28 22:27:19 +0530

    feat: better virtual text diagnostics

---
+++ lua/config/plugin/quality_of_life/tiny_diagnostics.lua
@@ -0,0 +1,24 @@
+--[[ better inline diagnostics ]]
+
+return {
+	"rachartier/tiny-inline-diagnostic.nvim",
+	name = "tiny_inline_diagnostics",
+	priority = 1000,
+	config = function()
+		vim.diagnostic.config({ virtual_text = false }) -- disable vim diagnostics first
+
+		local tiny = require("tiny-inline-diagnostic")
+		tiny.setup({
+			options = {
+				multilines = true,
+				overflow = { mode = "wrap" },
+			},
+		})
+
+		NKeymap(
+			"<leader>tt",
+			tiny.toggle,
+			{ desc = "plugins/tiny_inline_diagnostics: toggle tiny diagnostics" }
+		)
+	end,
+}
